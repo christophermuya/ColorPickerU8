5425c48216e7289c874024ebb268ddf9334bb602
Christopher Muya <chrismuya2012@gmail.com>
2019-12-20 16:21:02 -0500

    Fixed faulty file path. Improved design.

--- ColorPickerU8/ColorPickerController.js
+++ ColorPickerU8/ColorPickerController.js
@@ -5,30 +5,38 @@
  */
 angular.module("umbraco").controller("U8Muya.ColorPickerController",
     function spectrumColorPicker($scope, assetsService, angularHelper, $element) {
+
         // Check that rgb is selected if transparency is enabled.
         $scope.model.config.enableTransparency = convertToBoolean($scope.model.config.enableTransparency);
         if ($scope.model.config.enableTransparency) {
             $scope.model.config.preferredFormat = "rgb";
         }
-        assetsService.loadJs(
-            "~/App_Plugins/ColorPickerU8/lib/spectrum/spectrum.js"
-        ).then(function () {
+
+        $scope.showPaletteOnly = convertToBoolean($scope.model.config.showPaletteOnly);
+
+        $scope.showColorValue = convertToBoolean($scope.model.config.showColorValue);
+
+        assetsService.loadJs("/App_Plugins/ColorPickerU8/lib/spectrum/spectrum.js", $scope).then(function () {
             var predefinedColor = [];
             for (i = 0; i < $scope.model.config.predefinedColor.length; i++) {
                 predefinedColor.push($scope.model.config.predefinedColor[i].value);
-            }      
+            }
             $element.find("input").spectrum({
+                containerClassName: 'sp-boxCustom',
                 color: $scope.model.value,
                 cancelText: $scope.cancelText,
-                chooseText: $scope.chooseText,
+                chooseText: "select",
+                replacerClassName: "sp-replacerCustom",
                 togglePaletteMoreText: $scope.moreText,
                 togglePaletteLessText: $scope.lessText,
                 clearText: $scope.clearText,
                 noColorSelectedText: $scope.selectedText,
                 preferredFormat: $scope.model.config.preferredFormat,
                 showAlpha: $scope.model.config.enableTransparency, // Cannot be null.
+                hideAfterPaletteSelect: true,
                 showInitial: true,
-                showInput: true,
+                showInput: $scope.showColorValue,
+                showPaletteOnly: $scope.showPaletteOnly,
                 showPalette: true,
                 allowEmpty: true,
                 palette: predefinedColor,
